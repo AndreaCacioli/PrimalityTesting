136f00a6069fd86b1e6ffcb6a8694a4e25cecc21
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2022-05-10 12:24:33 +0200

    Newton added precision control

--- PerfectPower/PerfectPower.c
+++ PerfectPower/PerfectPower.c
@@ -11,12 +11,15 @@ int main()
     mpz_init(x);
     mpz_init(res);
 
-    mpz_set_ui(x, 100);
-    mpz_set_ui(res, 10);
+    mpz_set_ui(x, 50);
+    unsigned long root = 10; 
 
-    int_root(res, x, 2);
+    int_root(res, x, root);
 
     gmp_printf("\nThe final result of the calculation is %Zd\n", res);
+    gmp_printf("%Zd ^ %d = ", res, root);
+    mpz_pow_ui(res, res, root);
+    gmp_printf("%Zd\n", res);
 
     return 0;
 }
@@ -30,42 +33,49 @@ int main()
  */
 void int_root(mpz_t res, mpz_t x, unsigned long n)
 {
+    if(n == 0 || n == 1)
+    {
+        mpz_set(res, x);
+        return;
+    } 
+
     const int precision = 1024;
+    mpf_t solution_precision;
     mpf_t xk;
     mpf_t y;
     mpf_t x_const;
     mpf_t tmp;
+    mpf_t diff;
 
     mpf_init2 (xk, precision);
     mpf_init2 (y, precision);
     mpf_init2 (x_const, precision);
     mpf_init2(tmp, precision);
+    mpf_init2(diff, precision);
+    mpf_init2(solution_precision, precision);
 
+    mpf_set_d(solution_precision, 0.5); //we stop when we reach 1/2 precision
+    mpf_set_d(diff, 1000);
     mpf_set_z(xk,x);
     mpf_set_z(x_const,x);
 
-    mpz_t xk_intpart, y_intpart; //The integer parts of the terms in the succession
-    mpz_init_set_ui(xk_intpart, 0);
-    mpz_init_set_ui(y_intpart, 1);
     int i = 0;
 
-    while(mpz_cmp(xk_intpart,y_intpart) != 0)
+    while(mpf_cmp(diff, solution_precision) <= 0)
     {
         //Now we calculate the new value of x, but first we store the old one in y
         mpf_set(y,xk);
-        //new value of xk will be [(n-1)xk - x/(xk^(n-1))] / n
-        mpf_mul_ui(xk, xk, n-1); //xk = xk * n-1
+        //new value of xk will be [(n-1)xk + x/(xk^(n-1))] / n
         mpf_pow_ui(tmp, xk, n-1); //tmp = xk^(n-1)
         mpf_div(tmp, x_const,tmp); //tmp = x / tmp
-        mpf_sub(xk, xk, tmp); //xk = xk - tmp
+        mpf_mul_ui(xk, xk, n-1); //xk = xk * n-1
+        mpf_add(xk, xk, tmp); //xk = xk + tmp
         mpf_div_ui(xk, xk, n); //xk = xk / n
 
-        //Now we update the int parts
-        mpz_set_f(xk_intpart,xk);
-        mpz_set_f(y_intpart, y);
         i++;
 
-        gmp_printf("The %dth term of the succession is: %.*Ff\n", i, xk);
+        gmp_printf("The %dth term of the succession is: %Ff\n", i, xk);
+        mpf_sub(diff, y, xk); //diff = y - xk;
 
     }
     mpz_set_f(res, xk); //This function also truncates the number
